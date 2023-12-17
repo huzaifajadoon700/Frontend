d602aa71e968247405c16268efac1b9adba7c4e3
Huzaifa Jadoon <145706459+huzaifajadoon700@users.noreply.github.com>
2023-12-17 22:53:02 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const url = "https://backend1-api.vercel.app";
 
 export const addproduct =async (productdata)=>{
     console.log(productdata);
- return await axios.post(`https://backend1-api.vercel.app/shoes/shoes`,productdata);
+ return await axios.post(`https://backend1-api.vercel.app/shoes`,productdata);
  }
 export const addsignup =async (signupdata)=>{
     console.log(signupdata);
@@ -20,7 +20,7 @@ return await axios.post(`${url}/checkout`,checkouttdata);
 
 
 export const getshoes= async ()=>{
-return await axios.get(`https://backend1-api.vercel.app/shoes/viewproducts`)
+return await axios.get(`https://backend1-api.vercel.app/viewproducts`)
 }
 
 export const deleteShoe = async (productId) => {
