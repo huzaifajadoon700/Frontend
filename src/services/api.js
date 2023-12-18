27a4acbfdbbd91f12457996e12e380ef03079610
Huzaifa Jadoon <145706459+huzaifajadoon700@users.noreply.github.com>
2023-12-18 14:37:10 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,10 +4,18 @@ const url = "https://backend1-api.vercel.app";
 
 
 
-export const addproduct =async (productdata)=>{
-    console.log(productdata);
- return await axios.post(`${url}/shoes`,productdata);
- }
+export const addproduct = async (productdata) => {
+  try {
+    console.log("Product Data:", productdata);
+    const response = await axios.post(`${url}/shoes`, productdata);
+    console.log("Response:", response.data);
+    return response.data;  // You might want to return some data if needed
+  } catch (error) {
+    console.error("Error adding product:", error.message);
+    throw error;  // Rethrow the error to propagate it to the calling function/component
+  }
+};
+
 export const addsignup =async (signupdata)=>{
     console.log(signupdata);
  return await axios.post(`${url}/signup`,signupdata);
